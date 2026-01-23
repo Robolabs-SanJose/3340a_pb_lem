4c991fa6a731b0036cb9ebb42f142863c620f8f2
Uijin Cho <robolabssanjose@gmail.com>
2026-01-22 18:42:56 -0800

    fixed code for comp 1/22

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-pros::MotorGroup leftMotors({-8, 2, -3},
+pros::MotorGroup leftMotors({-8, -20, -3},
                             pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
-pros::MotorGroup rightMotors({5, 4, 13}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
+pros::MotorGroup rightMotors({5, 4, 7}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
 
 // Inertial Sensor on port 10
-pros::Imu imu(10);
+pros::Imu imu(18);
 
 pros::Motor Intakeb(-1);
-pros::Motor Intakef(10);
+pros::Motor Intakef(6);
 
 pros::ADIDigitalOut Descore('B');
 pros::ADIDigitalOut Matchload('A');
@@ -135,62 +135,57 @@ void competition_initialize() {}
 ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 
 /**
- * Runs during auto
+ * Runs during auto;
  *
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
+    chassis.setPose(0, 0, 0);
+    // chassis.moveToPose();
+    //  Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
+    chassis.moveToPose(65, -20, 0, 4000);
     // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
+    // chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
     // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
+    // chassis.waitUntil(10);
+    // chassis.cancelMotion();
     // Turn to face the point x:45, y:-45. Timeout set to 1000
     // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
+    // chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
     // Turn to face a direction of 90º. Timeout set to 1000
     // will always be faster than 100 (out of a maximum of 127)
     // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
+    // chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
     // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
     // following the path with the back of the robot (forwards = false)
     // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
+    // chassis.follow(example_txt, 15, 4000, false);
     // wait until the chassis has traveled 10 inches. Otherwise the code directly after
     // the movement will run immediately
     // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
+    // chassis.waitUntil(10);
+    // pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
     // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    // chassis.waitUntilDone();
+    // pros::lcd::print(4, "pure pursuit finished!");
 }
 
 /**
  * Runs in driver control
  */
 
- //DRIVER CONTROL
+// DRIVER CONTROL
 
-
-
-
-//driver controls:
+// driver controls:
 
 void set_both(int32_t voltage) {
     Intakef.move_voltage(voltage);
     Intakeb.move_voltage(voltage);
 }
 
-void set_intakeb(int32_t voltage) { 
-    Intakeb.move_voltage(voltage); 
-}
+void set_intakeb(int32_t voltage) { Intakeb.move_voltage(voltage); }
 
-void set_intakef(int32_t voltage) { 
-    Intakef.move_voltage(voltage); 
-}
+void set_intakef(int32_t voltage) { Intakef.move_voltage(voltage); }
 
 void toggle_matchload() {
     matchload_state = !matchload_state;
@@ -202,7 +197,6 @@ void toggle_descore() {
     Descore.set_value(descore_state);
 }
 
-
 void opcontrol() {
     // controller
     // loop to continuously update motors
@@ -213,27 +207,19 @@ void opcontrol() {
         // move the chassis with curvature drive
         chassis.arcade(leftY, rightX);
 
-        //BUTTON CONTROL:
-        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) { 
-            toggle_descore(); 
-        }
-        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) { 
-            toggle_matchload(); 
-        }
-
+        // BUTTON CONTROL:
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) { toggle_descore(); }
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) { toggle_matchload(); }
 
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
             set_intakef(12000);
 
-        } 
-        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
             Intakef.move_velocity(-200);
             Intakeb.move_velocity(-50);
-        } 
-        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
             set_both(12000);
-        } 
-        else {
+        } else {
             set_both(0);
         }
 
